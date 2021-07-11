8135b058c0fcaabb4efcd21490233eef2e27c848
Edgar-Zamora <edgarzamora2012@hotmail.com>
2021-07-10 18:11:21 -0700

    Creating package website.

--- R/get_appearance.R
+++ R/get_appearance.R
@@ -2,10 +2,13 @@
 #'
 #' @param access_token Required. Unique token obtained from the SuperHero API site. Token should be set to the environment and will be retrieved
 #' that way.
-#' @param character_id Required. Required. The \href{https://superheroapi.com/ids.html}{Character ID} or name of the Superhero
+#' @param character_id Required. The Character ID or name of the Superhero
 #'
 #' @return Returns a dataframe with the work/occupation  of the specific superhero.
 #' @export
+#' @example get_appearance('Batman')
+#'
+#'
 #'
 
 
