5a6ca829dc58734f9095131d36084a10cc36a893
Edgar-Zamora <edgarzamora2012@hotmail.com>
2021-07-11 19:41:56 -0700

    dataframe descriptions

---
+++ R/character_ids.R
@@ -0,0 +1,12 @@
+#' Characters associated with each id.
+#'
+#' A dataset containing the ids associated with each superhero
+#'
+#' @format A data frame with 731 rows and 2 variables:
+#' \describe{
+#'   \item{id}{unique id for ech character}
+#'   \item{character}{name of the superhero}
+#'   ...
+#' }
+
+"character_ids"
